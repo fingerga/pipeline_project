c96684c45fe9117b36ad12c32a1505b8151118c8
Grace Finger <fingerga@icloud.com>
2026-02-22 12:59:52 -0600

    0222 cleaned up comments

--- blast_assembly.py
+++ blast_assembly.py
@@ -1,9 +1,4 @@
-#take in a multi fasta file and:
-    #finds the longest (first) contig
-    #blasts it against database
-    #prints first 5 best matches and corresponding column names/data
-
-
+#importing requirec packages
 import sys
 import argparse
 from Bio import SeqIO
@@ -32,10 +27,10 @@ with open(infile, "r") as handle:
     for record in SeqIO.parse(handle, "fasta"): #using SeqIO to parse file
         ids += [record.id]
         seqs += [record.seq] #pulling out each sequence and adding to the lsit
-longest_contig = seqs[0]
-longest_contig = SeqRecord(Seq(longest_contig), id = ids[0])
+longest_contig = seqs[0] #first contig is longest based on how SPADES orders its output
+longest_contig = SeqRecord(Seq(longest_contig), id = ids[0]) #making a SeqRecord with sequence and ID
 
-with open(outfile, 'w') as o:
+with open(outfile, 'w') as o: #writing output to outfile
     SeqIO.write(longest_contig, o, "fasta")
 
 #blastn -query assembles -db {blast_db} -max_hsps 1 -out {output.blast_out} -outfmt "6 sacc pident length qstart qend sstart send bitscore evalue stitle"
